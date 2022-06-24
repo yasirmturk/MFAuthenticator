46c7f7b95b80adc38ac024e1252985caa1a5f2c6
Yasir Turk <yasir.turk@check24.de>
2022-06-24 11:57:26 +0200

    Initial Commit

---
+++ C24Authenticator/C24AuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  C24AuthenticatorApp.swift
+//  C24Authenticator
+//
+//  Created by Yasir Turk on 24.06.22.
+//
+
+import SwiftUI
+
+@main
+struct C24AuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C24AuthenticatorUITests/C24AuthenticatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  C24AuthenticatorUITestsLaunchTests.swift
+//  C24AuthenticatorUITests
+//
+//  Created by Yasir Turk on 24.06.22.
+//
+
+import XCTest
+
+class C24AuthenticatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
